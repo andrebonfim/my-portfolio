c5391fbda19b112111ec624ebae5d059d14ab1ca
André Luis Bonfim <andrebonfim@pm.me>
2025-12-31 15:19:41 -0300

    feat: Initial project structure with Flask

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route for the home page
+@app.route('/')
+def home():
+    # Instead of returning text, we render a template
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    # Debug mode allows auto-reload when you change code
+    app.run(debug=True)
